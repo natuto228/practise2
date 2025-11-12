8455d32f644960258c9a2f0f90622a2c20753a8c
242909071093-13 <242909071093-13@A203WS24>
2025-11-12 09:50:26 +0300

    Добавьте файлы проекта.

---
+++ работа 1/FileName.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale (0,"");
+	int a, b, S;
+	cout<<"¬ведите две переменные, €вл€ющиес€ целыми числами"<<endl;
+	cout<<"ѕеременна€ 1:";
+	cin>>a;
+	cout<<"ѕеременна€ 2:";
+	cin>>b;
+		S = a+b;
+	cout << "—умма, S=" << S << endl;
+	return 0;
+}
